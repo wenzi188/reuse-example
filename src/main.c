1cd865bbbc7841f8a7c16668dafaa24ba43ac2dc
Reinhardt <reinhardt@teasy.at>
2022-04-25 09:14:36 +0200

    changed license

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 /*
  * SPDX-FileCopyrightText: 2019 Jane Doe <jane@example.com>
  *
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-License-Identifier: MIT OR GPL-2.0
  */
 
 #include <stdio.h>
